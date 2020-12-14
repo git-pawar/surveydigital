0b80d0ce2a0eed3426322426d4f7339a57a9a1b6
Priyansha <66458990+priyansha-priya@users.noreply.github.com>
2020-12-14 18:04:13 +0530

    design update

--- resources/views/Report/partwise.blade.php
+++ resources/views/Report/partwise.blade.php
@@ -7,11 +7,13 @@
 </style>
 <div class="maininnersection">
     <div class="w-100 mb-2 row mx-0">
-        <label for="ward_no" class="labelinput">Ward No - <span
-                class="mx-1">{{str_pad($user->wards->ward_no,2,0,STR_PAD_LEFT)}}</span></label>
+        <!-- <label for="ward_no" class="labelinput">Ward No - <span
+                class="mx-1">{{str_pad($user->wards->ward_no,2,0,STR_PAD_LEFT)}}</span></label> -->
 
         <form method="get" action="{{route('report.typewise',['type'=>'partwise'])}}">
-            <select class="form-control selectinput" name="searchq" onchange="this.form.submit()">
+        <div class="row mx-0">
+        <div class="width-50">
+           <select class="form-control selectinput mb-1" name="searchq" onchange="this.form.submit()">
                 <option value="">Select Part No </option>
                 @if (count($parts))
                 @foreach ($parts as $item)
@@ -19,8 +21,47 @@
                     @endif>{{$item->part_no}}</option>
                 @endforeach
                 @endif
+            </select>
+        </div>
+        <div class="width-50">
+              <select class="form-control selectinput " name="searchq" onchange="this.form.submit()">
+                <option value="">filter By </option>
+                <option>Part Wise</option>
+                <option>Name Wise</option>
+                 <option> House No Wise</option>
+                 <option>Category Wise</option>
+                 <option>Color Wise</option>
+                 <option>Mobile No wise </option>
+                 <option>Voter list</option>
 
             </select>
+       </div>
+
+        </div>
+    <div class="w-100 row mx-0 mt-2 padding_10">
+            <div class="width-80">
+                <div class="form-row mb-2" data-validate="Name required">
+
+                    <input type="search" id="search1" value="" placeholder="Search name" name="searchq" class="partwise form-control mb-0 validate_this">
+                </div>
+            </div>
+            <div class="width-20 py-0 margin-1">
+                <button type="submit" class="btn_search submit_button" ><i class="fas fa-search"></i></button>
+            </div>
+
+       </div>
+
+       <div class="w-100 row mx-0 mt-2">
+            <div class="width-80">
+               <select class="form-control selectinput validate_this" >
+                <option value="">Category </option>
+             </select>
+        </div>
+          <div class="width-20 py-0 m-t-5">
+                <button  class="btn_search submit_button" ><i class="fas fa-search"></i></button>
+         </div>
+
+      </div>
         </form>
     </div>
     <div class="table-responsive" id="surveyorList">
